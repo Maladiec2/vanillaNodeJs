28c21ba90c35a65515bb0ad50b5f4789cbf22620
Ernestas Gr <e.grigonis58@gmail.com>
2020-10-22 16:22:59 +0300

    implemented mvc

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require("http")
+const { getProducts } = require('./controllers/productController')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200{'Content-Type': 'application/json'})k
-    res.end(JSON.stringify(products))
+    if (req.url === '/api/products') {
+        getProducts(req, res)
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ message: 'not found' }))
+    }
 })
 
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+
