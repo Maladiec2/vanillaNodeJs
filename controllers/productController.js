7da344bb78d7225444d44a984ee50539b5a57b21
Ernestas Gr <e.grigonis58@gmail.com>
2020-10-22 16:57:40 +0300

    find all and fint by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,5 @@
 const Product = require('../models/productModel')
-
+// gets all products
 async function getProducts(req, res) {
     try {
         const products = await Product.findAll()
@@ -9,6 +9,26 @@ async function getProducts(req, res) {
         console.log(error)
     }
 }
+
+// gets products by id
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id)
+
+        if(!product) {
+            res.writeHead(400, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Product not found'}))
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(product))
+        }
+
+    } catch {
+        console.log(error)
+    }
+}
 module.exports = {
-    getProducts
+    getProducts,
+    getProduct
 }
--- models/productModel.js
+++ models/productModel.js
@@ -5,7 +5,14 @@ function findAll() {
         resolve(products)
     })
 }
+function findById(id) {
+    return new Promise((resolve, reject) => {
+        const product = products.find((p) => p.id === id)
+        resolve(product)
+    })
+}
 
 module.exports = {
-    findAll
+    findAll,
+    findById
 }
